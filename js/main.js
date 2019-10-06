5b57a364e7a4aaa197ebc2dc5e004218ad2d88a5
Daniel Fazlijevic <danielfazlijevic98@gmail.com>
2019-10-06 02:17:41 +0200

    random name genrisanje

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-
 // DODATI: 
 // Promena radionice
 // Lista svih aktivnih radionica
 // Brisanje komentara
 // Profili
 // mozda pozivanje apija za neki gif na startu?
+// mozda random ime da se napravi i da se dodeli?
 
 var database = firebase.database();
 
@@ -13,8 +13,9 @@ const sendBtn = document.querySelector('#posalji');
 const questionList = document.querySelector('.pitanja ul');
 
 let radionica;
+let randomName;
 
-function submitCode(){
+function submitCode() {
     console.log('test');
     radionica = document.querySelector('#sifra').value;
     localStorage.setItem("radionica", radionica)
@@ -23,33 +24,36 @@ function submitCode(){
 
 
     startListening();
-   
-  
+
+
 }
 
-function startListening(){
+function startListening() {
     const questionsRef = firebase.database().ref(radionica + '/questions');
-    questionsRef.on('child_added', function(data) {
+    questionsRef.on('child_added', function (data) {
         console.log(data);
-        addQuestion(data);
-  });
+        addQuestion(data.val());
+    });
 
 }
 
 sendBtn.addEventListener("click", sendQuestion);
 
 
-function addQuestion(data){
-    const question = data.val().question;
-    const date = new Date(data.val().date).toLocaleTimeString('sr');;
-    const newQuestionElement = `<li>${question}, ${date}</li>`;
+function addQuestion(data) {
+    const question = data.question;
+    const date = new Date(data.date).toLocaleTimeString('sr');
+    const author = data.author;
+    const newQuestionElement = `<li>${author}: ${question}, ${date}</li>`;
     questionList.innerHTML += newQuestionElement;
 }
+
 function sendQuestion() {
     const question = document.querySelector('#pitanje').value;
     console.log('Pitanje: ', question);
 
-    firebase.database().ref(radionica + '/questions').push({    
+    firebase.database().ref(radionica + '/questions').push({
+        author: randomName,
         question: question,
         date: firebase.database.ServerValue.TIMESTAMP
     }, function (error) {
@@ -57,6 +61,32 @@ function sendQuestion() {
             alert("Doslo je do greske!")
         } else {
             alert("pitanje poslato baki");
+            document.querySelector('#pitanje').value = "";
         }
     });
 }
+
+
+function generateRandomName() {
+    const API_URL = "https://namey.muffinlabs.com/name.json";
+    fetch(API_URL, {
+        mode: 'cors',
+        method: "GET",
+        headers: {
+           "Content-Type": "application/json"
+        }
+    }).then(res => res.json()).then(data => {
+        randomName = data[0];
+        document.querySelector('#ime').textContent = randomName;
+    })
+    // link za console error i to 
+    .catch(err => {
+        console.log('ERROR', err)
+       randomName =  "Anon"
+       document.querySelector('#ime').textContent = randomName;
+    });
+    
+
+}
+
+generateRandomName();
